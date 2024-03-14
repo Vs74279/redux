2a31034f41f658efbc10af19e9367d04c09ac455
vikash kumar singh <drv74279@gmail.com>
2024-03-14 11:44:40 +0530

    Setting up a React Application for Redux

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Shop from './components/Shop';
+function App() {
+  return (
+    <>
+      <NavBar/>
+    <div className="container">
+    
+      <Shop/>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h1> Buy Nike Shoes 899 Rs </h1>
+      <button className='btn btn-primary mx-2'>-</button>
+      Add to cart
+      <button className='btn btn-primary mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
